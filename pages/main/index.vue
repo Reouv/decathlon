cd13203958a2e1f5cdd2e86286ba631303748b1c
unknown <utilg13@IUTPARIS.LOCAL>
2019-12-06 06:45:48 +0100

    add

---
+++ pages/main/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <b-jumbotron header="DecathSol :" lead="C'est claqué au sol" style="margin-top: 10vh; margin-left: 5vw;margin-right: 5vw">
+            <div class="container">
+                <p class="lead">
+                Mélanger le sport aux nouvelles technologies.
+                    Attirer et faire découvrir des sports à ceux qui ne les ont jamais pratiqués.
+                    Vivre une expérience unique et hors du commun.
+
+                <strong>Notre projet consiste à digitaliser l’environnement des activités sportifs de terrains.</strong>
+
+                    La digitalisation commence par le changement du revêtement en remplaçant le revêtement initial par un revêtement de LED et de capteur, il est aussi possible d’ajouter une couleur fluorescente sur les ballons, et les volants etc.. Ces changements indiqueront les bordures de terrains, le temps de jeu, le score, les fautes de jeu, les placements, ainsi que des animations qui dynamiseront le jeu dans le but de le rendre plus vivant.
+                    Prenons l’exemple du volley-ball :
+                    -	Le projet permettra au joueur de tout type de niveau de s’améliorer et de tester le terrain. En réalisant des sessions d’entrainement où le joueur aura des cibles sur lesquelles il pourra smasher et des informations sur son positionnement, toutes ces fonctionnalités mettront en avant les qualités de ce sport et permettra une bonne première approche du volley-ball.
+
+                    -	Le terrain devrait être disponible en magasin afin que les non connaisseurs de sports de terrain puissent découvrir ces sports d’un point de vue différents que le point de vue « commun » que l’on peut avoir à propos de ce sport.
+                </p>
+
+            </div>
+        </b-jumbotron>
+        <h1></h1>
+
+    </div>
+
+
+</template>
